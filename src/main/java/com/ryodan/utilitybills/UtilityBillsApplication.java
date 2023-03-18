6a22542acdd4aa1732a28c791cec82684f360fb2
Necroblaster <awesome.semenov@gmail.com>
2023-03-18 14:57:04 +0400

    Initial commit

---
+++ src/main/java/com/ryodan/utilitybills/UtilityBillsApplication.java
@@ -0,0 +1,13 @@
+package com.ryodan.utilitybills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityBillsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilityBillsApplication.class, args);
+    }
+
+}
