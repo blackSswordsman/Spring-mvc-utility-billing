242d6bbd8a4f9410084973ca4ecd8a76a4709ed5
Rinexus <94224251+Rinexus@users.noreply.github.com>
2023-04-15 15:11:09 +0400

    Dev.s (#6)
    * application.yml reconfigured
    
    * Added and configured JPA and etc
    
    * Added repos, first controller and page
    
    * CommonRepository added, with rest controller for AccommodationType

---
+++ src/main/java/com/ryodan/utilitybills/repository/AccommodationTypeRepository.java
@@ -0,0 +1,8 @@
+package com.ryodan.utilitybills.repository;
+
+import com.ryodan.utilitybills.pojo.AccommodationType;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AccommodationTypeRepository extends CommonRepository<AccommodationType> {
+}
